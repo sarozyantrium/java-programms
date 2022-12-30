07bf2420be7666201e0a7221fadb336cdd40607a
HastagKc <chaudharykshittiz950@gmail.com>
2022-12-30 08:28:36 +0545

    throw in try catch

--- tryCatchInJava/tryCatch/src/TryCatch.java
+++ tryCatchInJava/tryCatch/src/TryCatch.java
@@ -1,5 +1,20 @@
-public class TryCatch {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class TryCatch {
+    // ? creating function name checkAge()
+    static void checkAge(int age) {
+        // ANCHOR - throw is used to create custom exception
+        if (age <= 18) {
+            throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+        } else {
+            System.out.println("Access granted - You are old enough!");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your age: ");
+        int age = sc.nextInt();
+        checkAge(age);
     }
 }
